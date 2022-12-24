267f0f6f293405e98bcc0f1fe0806298a8a43ba2
Nathandisk <103308730+Nathandiassoares@users.noreply.github.com>
2022-12-24 20:20:26 -0300

    Teste

--- app/main.js
+++
@@ -1,7 +0,0 @@
-const boxImagenOnca = document.querySelector('[data-test01]')
-const iniciaJogo = document.querySelector('.subtitulo')
-
-
-iniciaJogo.addEventListener('click', elemento => {
-    boxImagenOnca.setAttribute('src','app/onca.png')
-})
